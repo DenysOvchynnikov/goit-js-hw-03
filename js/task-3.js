bc159155d6878ace5733e934946ae6121a8e1ec1
Denys <benden396@gmail.com>
2025-03-25 23:24:57 +0200

    finish task

---
+++ js/task-3.js
@@ -0,0 +1,36 @@
+// function filterArray(numbers, value) {
+//     let newArray = []
+//     for(let i = 0; i <= numbers.length; i ++ ) {
+//         if(numbers[i] > value) {
+//             newArray.push(numbers[i])
+//         }
+//     }
+//     return newArray
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// function filterArray(numbers, value) {
+//     let newArray = []
+//     for(i of numbers) {
+//         if(i > value) {
+//             newArray.push(i)
+//         }
+//     }
+//     return newArray
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+const filterArray = (numbers, value) => numbers.filter((elm) => elm > value)
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
